ad6272f7c653f5e2c0f45b4455ec27aeda09f981
serg <nautbek@gmail.com>
2023-07-22 11:09:32 +0300

    Fix header and description

--- Modules/Share/Config/config.php
+++ Modules/Share/Config/config.php
@@ -4,7 +4,7 @@ return [
     'phone'       => '8 950 838 48 23',
     'email'       => 'prosamocaty@gmail.com',
     'address'     => 'г. Ижевск',
-    'description' => "Интернет магазин запчастей для электросамокатов в Ижевске. Большинство товаров есть в наличии.",
+    'description' => "Интернет магазин запчастей и аксесуаров для электросамокатов в Ижевске. Большинство деталей есть в наличии.",
 
     /*
      * These factories is used when you run `db:seed` command, for set new factory you can go to related service provider
